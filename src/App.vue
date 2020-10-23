1a739fc6df7b8cc7e3e716ef74e969a7eefec4fd
CatAnderson <thecatanderson100@gmail.com>
2020-10-23 23:56:47 +0100

    added TvShowList component, trying to get @click to work on image

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
-<template>
-<main>
-  <h1>New TV show app!!</h1>
-  <ul 
-  <li v-for='(show, index) in shows' :key='index' :value='index'> {{ show.name }} </li>
-
-  </ul>
-</main>
+<template lang='html'>
+  <body>
+    <header>
+      <h1>TV show app!!</h1>
+    </header>
+    <main>
+      <tv-show-list v-if='shows.length' :shows='shows'></tv-show-list>
+    </main>
+    <footer>
+      created by Clementine Studios.
+    </footer>
+  </body>
 </template>
 
 <script>
+import TvShowList from "./components/TvShowList.vue"
+
+import { eventBus } from '@/main.js';
+
 export default {
   name: 'App',
 
   data(){
     return{
       shows: [],
-      selectedShow: null
+      selectedTvShow: null
     }
   },
 
@@ -23,10 +31,19 @@ export default {
     fetch('http://api.tvmaze.com/shows')
     .then(response => response.json())
     .then(data => this.shows = data);
+
+    eventBus.$on('selected-tv-show', (show) => {
+      this.selectedTvShow = show
+    })
+  },
+
+  components: {
+    TvShowList,
   }
+    
 }
 </script>
 
-<style>
+<style lang='css' scoped>
 
 </style>
---
+++ src/components/TvShowList.vue
@@ -0,0 +1,32 @@
+<template lang='html'>
+<main>
+  <h2> pick a show... </h2>
+  <img @click='handleClick' id="selected-tv-show" v-for='(show, index) in shows' :key='index' :value='index' :src="show.image.medium" alt="tv show poster">
+</main>  
+</template>
+
+<script>
+import { eventBus } from '@/main.js';
+
+export default {
+    name: 'tv-show-list',
+    props: ['shows'],
+
+    data(){
+        return{
+            selectedTvShow: null
+        }
+    },
+
+    methods:{
+        handleClick: function(){
+            eventBus.$emit('selected-tv-show', this.selectedTvShow)
+        }
+    }
+
+}
+</script>
+
+<style lang='css' scoped>
+
+</style>
